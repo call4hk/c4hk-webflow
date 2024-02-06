32b55312838b0e40edc698de588c457abc20e885
kabigon <snoxlax0kabigon@gmail.com>
2024-02-05 21:44:12 -0800

    bills update

--- bills.js
+++ bills.js
@@ -6,12 +6,24 @@ const CurrentBillsPaging = () => {
   return <div>Current bills paging</div>
 };
 
+const BillPill = (props) => {
+  const text = props.text;
+  return (
+    <div className='rounded-lg bg-rose-100 py-1 px-2'>
+      <span>{text}</span>
+    </div>
+  );
+};
+
 const CurrentBill = (props) => {
   const { bill } = props;
   return (
-    <div className='flex'>
-      <h2>{bill.bill_title}</h2>
-      <p>{bill.sponsor_name}</p>
+    <div className='flex flex-col mb-4 bg-white rounded-lg'>
+      <h2 className='text-2xl font-bold'>{bill.bill_title}</h2>
+      <p className='text-sm'>{bill.sponsor_name}</p>
+      <div className='flex flex-row'>
+        <BillPill text={bill.status} />
+      </div>
     </div>
   );
 };
@@ -36,7 +48,7 @@ const CurrentBillsContent = (props) => {
 const CurrentBillsPanel = (props) => {
   const { bills } = props;
   return (
-    <div className='flex'>
+    <div className='flex flex-col'>
       <BillsControl />
       <CurrentBillsContent bills={bills} />
       <CurrentBillsPaging />
@@ -59,7 +71,18 @@ const PreviousBillsPanel = (props) => {
 };
 
 const CurrentBills = () => {
-  const [allBills, setAllBills] = React.useState([]);
+  const [allBills, setAllBills] = React.useState([
+    {
+      bill_title: 'test title',
+      sponsor_name: 'Test Sponsor',
+      status: 'Intro to House',
+    },
+    {
+      bill_title: 'test title 2',
+      sponsor_name: 'Test Sponsor2',
+      status: 'Intro to House',
+    }
+  ]);
 
   React.useEffect(() => {
     getEndpoint('bills', 'current').then(bills => {
